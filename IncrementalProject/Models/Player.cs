d74103e43fb680dfbd4db65b58109d1dde1eff9f
RANJEET KUMAR <147934359+ranjeetkumar274@users.noreply.github.com>
2026-01-20 17:01:48 +0530

    Add Player model with properties

---
+++ IncrementalProject/Models/Player.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+
+namespace dotnetapp.Models
+{
+
+    public class Player{
+    public int Id{get; set;}
+
+    public string Name{get; set;}
+
+    public int Age{get; set;}
+
+    public string Category{get; set;}
+
+    public decimal BiddingPrice{get; set;}
+
+    public int TeamId{get; set;}
+}
+}
