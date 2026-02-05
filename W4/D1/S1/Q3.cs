4aeb269bb94b3f1d771879abf34c1d3a101426b3
Hrithik Reddy Chenreddy <122119750+hrxth-xk@users.noreply.github.com>
2026-02-05 16:40:42 +0530

    Refactor BookController to Razor page for Authors
    Removed BookController class and replaced with Razor page for Authors.

--- W4/D1/S1/Q3.cs
+++ W4/D1/S1/Q3.cs
@@ -40,43 +40,17 @@ namespace dotnetapp.Controllers
 //Authors.cshtml
  
  
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System.Runtime.InteropServices;
- 
-namespace dotnetapp.Controllers
-{
-    // [Route("[controller]")]
-    public class BookController : Controller
-    {
-        [Route("book/home")]
- 
-        public IActoinResult Home(){
-            return View();
-        }
-        [Route("book/books")]
- 
-        public IActoinResult Books(){
-            return View();
-        }
-        [Route("book/authors")]
- 
-        public IActoinResult Authors(){
-            return View();
-        }
-        [Route("book/categories")]
- 
-        public IActoinResult Categories(){
-            return View();
-        }
-    }
+@page
+@model dotnetapp.Views.Book.Authors
+@{
+    ViewData["Title"] = "Authors";
 }
  
+<h1>@ViewData["Title"]</h1>
+ 
+<div>
+   
+</div>
  
  
  
