47dd8750472a2e74b83a4b1b7857a7838a3f4a00
RANJEET KUMAR <147934359+ranjeetkumar274@users.noreply.github.com>
2026-02-06 12:48:41 +0530

    Create LoginRegister.cs

---
+++ IncrementalProject/Models/LoginRegister.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+ 
+namespace dotnetapp.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Email{get;set;}
+        [DataType(DataType.Password)]
+        public string Password{get;set;}
+       
+    }
+}
