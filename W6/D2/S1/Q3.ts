b78a8cb4d9b81ac67844db0035a7499d2d77a59c
RANJEET KUMAR <147934359+ranjeetkumar274@users.noreply.github.com>
2026-02-14 15:44:23 +0530

    Update Q3.ts

--- W6/D2/S1/Q3.ts
+++ W6/D2/S1/Q3.ts
@@ -7,35 +7,34 @@ import { PROMPTS } from 'src/prompts';
   styleUrls: ['./promptdisplay.component.css']
 })
 export class PromptdisplayComponent implements OnInit {
-  prompts: string[] = PROMPTS;
-  currentPromptIndex: number = 0;
-  prompt: string;
-
-  constructor() {
-    this.nextPromt();
+  prompts : string[]=PROMPTS;
+  currentPromptIndex:number=0;
+  prompt:string;
+  constructor(){
+    this.nextPrompt();
   }
-
-  nextPromt() {
-    if (this.prompts.length > this.currentPromptIndex) {
-      this.prompt = this.prompts[this.currentPromptIndex];
+  nextPrompt(){
+    if(this.prompts.length>this.currentPromptIndex)
+    {
+      this.prompt=this.prompts[this.currentPromptIndex];
       this.currentPromptIndex++;
-    } else {
-      this.currentPromptIndex = 0;
-      this.prompt = this.prompts[this.currentPromptIndex];
+
+    }
+    else{
+      this.currentPromptIndex=0;
+      this.prompt=this.prompts[this.currentPromptIndex];
     }
   }
 
-  ngOnInit(): void {}
-  
-  <div class="moal container">
-  <h1>Daily Prompts</h1>
-  
-  <p [innerText]="prompt"></p>
-  
-  <button (click)="nextPromt()">
-    Next
-  </button>
-</div>
+  ngOnInit():void{
+
+  }
+
+}
 
   
-}
+<div class="moal container">
+    <h1>Daily Prompts</h1>
+    <p [innerText]="prompt"></p>
+    <button (click)="nextPrompt()">Next</button>
+</div>
