a16bc2885d3fe4932615cb299511028b9e7afab3
RANJEET KUMAR <147934359+ranjeetkumar274@users.noreply.github.com>
2026-02-17 10:19:30 +0530

    Create attendee.component.ts file

---
+++ IncrementalProject/angularapp/src/app/attendee/attendee.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attendee',
+  templateUrl: './attendee.component.html',
+  styleUrls: ['./attendee.component.css']
+})
+export class AttendeeComponent {}
