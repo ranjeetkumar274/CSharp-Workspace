8256ed0a2106dcf50b9546d76131d881641cedf5
RANJEET KUMAR <147934359+ranjeetkumar274@users.noreply.github.com>
2026-02-17 10:19:10 +0530

    Create organizer.component.ts file

---
+++ IncrementalProject/angularapp/src/app/organizer/organizer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organizer',
+  templateUrl: './organizer.component.html',
+  styleUrls: ['./organizer.component.css']
+})
+export class OrganizerComponent { }
