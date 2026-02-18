010f7d239176ff41a55e28d572262263663cfc98
RANJEET KUMAR <147934359+ranjeetkumar274@users.noreply.github.com>
2026-02-18 09:07:10 +0530

    Implement AuthGuard for route protection

---
+++ IncrementalProject/Manvi/authguard.ts
@@ -0,0 +1,25 @@
+
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigate(['/login']);
+      return false;
+    }
+
+    if (!this.authService.isAdmin() && !this.authService.isOrganizer()) {
+      this.router.navigate(['/error']);
+      return false;
+    }
+
+    return true;
+  }
+}
