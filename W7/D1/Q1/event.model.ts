e1a11ca9207c832a4b659d16e00519b8b160795a
RANJEET KUMAR <147934359+ranjeetkumar274@users.noreply.github.com>
2026-02-19 13:14:53 +0530

    Create Event interface for event model

---
+++ W7/D1/Q1/event.model.ts
@@ -0,0 +1,10 @@
+export interface Event{
+    
+    eventId: number;
+    eventName: string;
+    eventDescription: string;
+    eventDate: string;
+    eventTime: string;
+    eventLocation: string;
+    eventOrganizer: string;
+}
